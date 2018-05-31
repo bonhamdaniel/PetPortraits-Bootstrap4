de3e3da210d191590307792f53519999a7aaccbd
Daniel Bonham <bonhamdaniel@gmail.com>
2018-05-31 09:34:41 -0400

    Fixed issues 1,3,4

--- js/kathryn.js
+++ js/kathryn.js
@@ -1,33 +1,30 @@
-"use strict";
-
 function picPage(num) {
-  var i;
-  var showing;
-  var hidden;
-  var hidden2;
-  if(num === 1 || (num === 0 && document.getElementById("picSet2").style.display == "block")) {
-    showing = document.getElementsByClassName("picSet1");
-    hidden = document.getElementsByClassName("picSet2");
-    hidden2 = document.getElementsByClassName("picSet3");
-  }
-  else if(num === 2 || (num === 0 && document.getElementById("picSet3").style.display == "block") || (num === 4 && document.getElementById("picSet1").style.display == "block")) {
-    hidden = document.getElementsByClassName("picSet1");
-    showing = document.getElementsByClassName("picSet2");
-    hidden2 = document.getElementsByClassName("picSet3");
-  }
-  else if(num === 3 || (num === 4 && document.getElementById("picSet2").style.display == "block")) {
-    hidden = document.getElementsByClassName("picSet1");
-    hidden2 = document.getElementsByClassName("picSet2");
-    showing = document.getElementsByClassName("picSet3");
-  }
-  for (i = 0; i < hidden.length; i++) {
-    hidden[i].style.display = "none";  
-  }
-  for (i = 0; i < hidden2.length; i++) {
-    hidden2[i].style.display = "none";  
-  }
-  for (i = 0; i < showing.length; i++) {
-    showing[i].style.display = "block";  
-  }
-  document.getElementById("gallery").focus();
+    'use strict';
+    var i, showing, hidden, hidden2,
+        ps1Display = document.getElementById("picSet1").style.display,
+        ps2Display = document.getElementById("picSet2").style.display,
+        ps3Display = document.getElementById("picSet3").style.display;
+    if (num === 1 || (num === 0 && ps2Display === "block")) {
+        showing = document.getElementsByClassName("picSet1");
+        hidden = document.getElementsByClassName("picSet2");
+        hidden2 = document.getElementsByClassName("picSet3");
+    } else if (num === 2 || (num === 0 && ps3Display === "block") || (num === 4 && ps1Display === "block")) {
+        hidden = document.getElementsByClassName("picSet1");
+        showing = document.getElementsByClassName("picSet2");
+        hidden2 = document.getElementsByClassName("picSet3");
+    } else if (num === 3 || (num === 4 && ps2Display === "block")) {
+        hidden = document.getElementsByClassName("picSet1");
+        hidden2 = document.getElementsByClassName("picSet2");
+        showing = document.getElementsByClassName("picSet3");
+    }
+    for (i = 0; i < hidden.length; i = i + 1) {
+        hidden[i].style.display = "none";
+    }
+    for (i = 0; i < hidden2.length; i = i + 1) {
+        hidden2[i].style.display = "none";
+    }
+    for (i = 0; i < showing.length; i = i + 1) {
+        showing[i].style.display = "block";
+    }
+    document.getElementById("gallery").focus();
 }
